8906b6c541ecbaa7418a1eb4f26b15d519c42ba3
DaniDevelopment98 <105693050+DaniDevelopment98@users.noreply.github.com>
2022-06-14 12:50:03 -0600

    Add files via upload

---
+++ Data/api_EjemploContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using api_Ejemplo.Models;
+
+namespace api_Ejemplo.Data
+{
+    public class api_EjemploContext : DbContext
+    {
+        public api_EjemploContext (DbContextOptions<api_EjemploContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<api_Ejemplo.Models.Clientes> Clientes { get; set; }
+    }
+}
